3d811a5a9cda39bf6c9e068df8773d733e6c4bff
oscarsanchez9 <62642382+oscarsanchez9@users.noreply.github.com>
2020-03-25 11:22:17 +0100

    Add files via upload

---
+++ ProyectoConjuto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProyectoConjuto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
