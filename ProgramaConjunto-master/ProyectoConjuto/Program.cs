d89b55c4325fa05a21345c3b3a5e414fd790f6eb
RaulG97 <raul.galindo@campusfp.es>
2020-04-15 09:46:51 +0200

    ClaseRaul y ClaseProgram

---
+++ ProgramaConjunto-master/ProyectoConjuto/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyectoConjuto
+{
+     class Program
+    {
+        static void Main()
+        {
+            ClaseRaul luz1 = new ClaseRaul();
+            luz1.AddDato();
+            luz1.MostrarAPartirDe();
+        }
+    }
+} 
